10b770f7334a9b407efdaa9a2b835a637f74b87c
cayleb247 <156046197+cayleb247@users.noreply.github.com>
2024-08-24 11:32:16 -0400

    Create calculator UI
    Created html and css files for creating calculator UI
    Created a display div
    Made multicolored buttons for numbers 0-9 and operations
    Included a clear button
    Imported Roboto Mono and Jersey 10 for buttons and display
    Began linking js to html with buttons

--- script.js
+++ script.js
@@ -13,3 +13,24 @@ const mult = function(a, b) {
 const divide = function(a, b) {
     return a / b;
 }
+
+const display = document.querySelector(".display");
+
+const oneBtn = document.querySelector(".button num")
+oneBtn.addEventListener("click", () => {
+    display.textContent = "1";
+})
+
+// let firstNum = 
+
+// let operator = 
+
+// let secondNum =
+
+// const operate = function(a, b, operator) {
+//     operator == "+" ? add(a, b)
+//     : operator == "-" ? sub(a,b)
+//     : operator == "*" ? mult(a, b)
+//     : operator == "/" ? divide(a, b)
+// }
+    
